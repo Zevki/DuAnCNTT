441b11a2c2bf79a7d57a17302c66a34e786af79a
Zenki <tqt2401@gmail.com>
2024-07-02 16:59:52 +0700

    add utils

---
+++ utils/types.d.ts
@@ -0,0 +1,52 @@
+export interface TruncateParams {
+  text: string;
+  startChars: number;
+  endChars: number;
+  maxLength: number;
+}
+
+export interface PollParams {
+  name: string;
+  info: string;
+  startTime: number | string;
+  endTime: number | string;
+}
+
+export interface PollStruct {
+  id: number;
+  name: string;
+  info: string;
+  voteCount: number;
+  contestantCount: number;
+  isDeleted: boolean;
+  creator: string;
+  startTime: number;
+  endTime: number;
+  createdTime: number;
+  images: string[];
+  voters: string[];
+}
+
+export interface ContestantStruct {
+  id: number;
+  picture: string;
+  contestantName: string;
+  participant: string;
+  voteCount: number;
+  voters: string[];
+}
+
+export interface GlobalState {
+  wallet: string;
+  createModal: string;
+  updateModal: string;
+  deleteModal: string;
+  contestModal: string;
+  polls: PollStruct[];
+  poll: PollStruct | null;
+  contestants: ContestantStruct[];
+}
+
+export interface RootState {
+  globalStates: GlobalState;
+}
